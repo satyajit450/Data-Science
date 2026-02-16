7c095a2e2672a5b0a312bd3fae6802c6a65f497f
SATYAJIT SAHU <satyajeetsahu53@gmail.com>
2026-02-16 23:03:04 +0530

    Comment out all code in main.py
    Comment out the entire content of main.py for future reference.

--- Machine_Learning/Guragaon/main.py
+++ Machine_Learning/Guragaon/main.py
@@ -1,87 +1,87 @@
-import os
-import joblib # type: ignore
-import numpy as np # type: ignore
-import pandas as pd# type: ignore
-import matplotlib.pyplot as plt# type: ignore
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder# type: ignore
-from sklearn.model_selection import StratifiedShuffleSplit# type: ignore
-from sklearn.impute import SimpleImputer# type: ignore
-from sklearn.pipeline import Pipeline # type: ignore
-from sklearn.compose import ColumnTransformer# type: ignore
-from sklearn.ensemble import RandomForestRegressor# type: ignore
+# import os
+# import joblib # type: ignore
+# import numpy as np # type: ignore
+# import pandas as pd# type: ignore
+# import matplotlib.pyplot as plt# type: ignore
+# from sklearn.preprocessing import MinMaxScaler, OneHotEncoder# type: ignore
+# from sklearn.model_selection import StratifiedShuffleSplit# type: ignore
+# from sklearn.impute import SimpleImputer# type: ignore
+# from sklearn.pipeline import Pipeline # type: ignore
+# from sklearn.compose import ColumnTransformer# type: ignore
+# from sklearn.ensemble import RandomForestRegressor# type: ignore
 
 
-MODEL_FILE = "model.pkl"
-PIPELINE_FILE = "pipeline.pkl"
+# MODEL_FILE = "model.pkl"
+# PIPELINE_FILE = "pipeline.pkl"
 
-def build_pipeline(num_attributes , cat_attributes) :
-
-
-    num_pipeline = Pipeline([
-        ("Imputer" , SimpleImputer(strategy="most_frequent")),
-        ("MinMax",MinMaxScaler(feature_range=(-1,1))),
-    ])
-    Cat_pipeline = Pipeline([
-    ("one_hot_encoder",OneHotEncoder(handle_unknown="ignore")),
-    ])
-
-    full_pipeline = ColumnTransformer ([
-        ("Num",num_pipeline,num_attributes),
-        ("Cat",Cat_pipeline,cat_attributes)
-    ])
-    return full_pipeline
+# def build_pipeline(num_attributes , cat_attributes) :
 
 
-if not os.path.exists(MODEL_FILE) :
-    #Read CSv
+#     num_pipeline = Pipeline([
+#         ("Imputer" , SimpleImputer(strategy="most_frequent")),
+#         ("MinMax",MinMaxScaler(feature_range=(-1,1))),
+#     ])
+#     Cat_pipeline = Pipeline([
+#     ("one_hot_encoder",OneHotEncoder(handle_unknown="ignore")),
+#     ])
 
-    housing = pd.read_csv("housing.csv")
-
-    housing["income_cat"] = pd.cut(housing["median_income"],bins=[0.0,1.5,3.0,4.5,6.0,np.inf],labels=[1,2,3,4,5])
-
-    ##StratifiedShuffleSplit
-
-    split = StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=42)
-
-    for train_index,test_index in split.split(housing,housing["income_cat"]) :
-        train_data = housing.loc[train_index]
-        housing.loc[test_index].drop("income_cat", axis=1).to_csv("input.csv",index=False)
+#     full_pipeline = ColumnTransformer ([
+#         ("Num",num_pipeline,num_attributes),
+#         ("Cat",Cat_pipeline,cat_attributes)
+#     ])
+#     return full_pipeline
 
 
-    train_data.drop(columns="income_cat", inplace=True)
+# if not os.path.exists(MODEL_FILE) :
+#     #Read CSv
+
+#     housing = pd.read_csv("housing.csv")
+
+#     housing["income_cat"] = pd.cut(housing["median_income"],bins=[0.0,1.5,3.0,4.5,6.0,np.inf],labels=[1,2,3,4,5])
+
+#     ##StratifiedShuffleSplit
+
+#     split = StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=42)
+
+#     for train_index,test_index in split.split(housing,housing["income_cat"]) :
+#         train_data = housing.loc[train_index]
+#         housing.loc[test_index].drop("income_cat", axis=1).to_csv("input.csv",index=False)
+
+
+#     train_data.drop(columns="income_cat", inplace=True)
 
     
-    housing = train_data.copy()
-    # print(train_data)
-    Categorical = ["ocean_proximity"]
-    Numerical = [col for col in housing.columns if col not in Categorical + ["median_house_value"]]
+#     housing = train_data.copy()
+#     # print(train_data)
+#     Categorical = ["ocean_proximity"]
+#     Numerical = [col for col in housing.columns if col not in Categorical + ["median_house_value"]]
 
-    housing_features = housing[Numerical + Categorical]
-    housing_labels = housing["median_house_value"]
+#     housing_features = housing[Numerical + Categorical]
+#     housing_labels = housing["median_house_value"]
 
 
-    # print(train_data)
+#     # print(train_data)
 
-    # print(housing_labels)
+#     # print(housing_labels)
 
-    pipeline = build_pipeline(Numerical,Categorical)
-    data_prepared = pipeline.fit_transform(housing_features)
+#     pipeline = build_pipeline(Numerical,Categorical)
+#     data_prepared = pipeline.fit_transform(housing_features)
 
-    model = RandomForestRegressor(random_state=42)
-    model.fit(data_prepared,housing_labels)
+#     model = RandomForestRegressor(random_state=42)
+#     model.fit(data_prepared,housing_labels)
 
-    joblib.dump(model,MODEL_FILE)
-    joblib.dump(pipeline,PIPELINE_FILE)
-    print("Model trained and saved.")
+#     joblib.dump(model,MODEL_FILE)
+#     joblib.dump(pipeline,PIPELINE_FILE)
+#     print("Model trained and saved.")
 
-else :
-    model = joblib.load(MODEL_FILE)
-    pipeline = joblib.load(PIPELINE_FILE)
+# else :
+#     model = joblib.load(MODEL_FILE)
+#     pipeline = joblib.load(PIPELINE_FILE)
 
-    input_data = pd.read_csv("input.csv")
-    tranformed = pipeline.transform(input_data)
-    prediction = model.predict(tranformed)
-    input_data["median_house_value"] = prediction
+#     input_data = pd.read_csv("input.csv")
+#     tranformed = pipeline.transform(input_data)
+#     prediction = model.predict(tranformed)
+#     input_data["median_house_value"] = prediction
 
-    input_data.to_csv("output.csv", index=False)
-    print("Inference complete. Results saved to output.csv")
+#     input_data.to_csv("output.csv", index=False)
+#     print("Inference complete. Results saved to output.csv")
