0b6ec56f5bcc853dddea26e8fa76e8a686d3778b
satyajit450 <satyajeetsahu53@gmail.com>
2026-02-16 22:56:22 +0530

    done

--- Deep_Learning/perceptron.py
+++ Deep_Learning/perceptron.py
@@ -5,7 +5,7 @@ def perceptron(x1,x2,w1,w2,b) :
     y = x1*w1+x2*w2+b 
     return step(y)
 
-print(perceptron(0, 0, 1, 1, -1.5))  # Expected: 0
-print(perceptron(0, 1, 1, 1, -1.5))  # Expected: 0
-print(perceptron(1, 0, 1, 1, -1.5))  # Expected: 0
+print(perceptron(0, 0, 1, 1, -1.5))  
+print(perceptron(0, 1, 1, 1, -1.5))  
+print(perceptron(1, 0, 1, 1, -1.5))  
 print(perceptron(1, 1, 1, 1, -1.5))  
