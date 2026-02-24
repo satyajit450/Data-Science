267c99668244f2dffe9c6193fd631589c27a8a2b
SATYAJIT SAHU <satyajeetsahu53@gmail.com>
2026-02-24 22:25:49 +0530

    Change MongoDB database and collection names
    Updated database and collection names for MongoDB connection.

--- Flask/main.py
+++ Flask/main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 # MongoDB Connection
 client = MongoClient("mongodb+srv://Satyajit:<password>@cluster0.8c20nzh.mongodb.net/")
-db = client["Details"]      # Database name
-collection = db["Emails_password"]          # Collection name
+db = client["Database name"]      # Database name
+collection = db["Collection name"]          # Collection name
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
